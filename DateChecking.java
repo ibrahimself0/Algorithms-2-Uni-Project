80e38478accf47b9c8495bcd07f04ca1c465e9a2
ali-mdaghmesh <alimdaghmesh116@gmail.com>
2025-06-14 18:39:26 +0300

    additional 1

---
+++ DateChecking.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+public class DateChecking {
+   
+        public static Boolean check(String date){
+        LocalDate currentDate=LocalDate.now(); 
+        String[] parts = date.split("[^0-9]+");
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Invalid date format");
+        }
+
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+        LocalDate deleviryDate=LocalDate.of(year,month,day);
+        if (currentDate.isBefore(deleviryDate)||currentDate.isEqual(deleviryDate)) {
+            return true; 
+        }
+        return false; 
+    }
+    
+}
--- ShipmentNode.java
+++ ShipmentNode.java
@@ -8,11 +8,11 @@ public class ShipmentNode {
     ArrayList<Product> Products ;
     ShipmentNode left, right;
 
-    public ShipmentNode(String destination, double cost, String deliveryDate,ArrayList<Product> Products ) {
+    public ShipmentNode(String destination, double cost, String deliveryDate,ArrayList<Product> Products ) {     
         this.destination = destination;
         this.cost = cost;
         this.deliveryDate = deliveryDate;
         left = right = null;
         this.Products = Products;
-    }
+    } 
 }
